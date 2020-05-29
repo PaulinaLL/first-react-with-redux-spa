5f2946f754f624da667b8f6e9ca21b40ac356605
PaulinaLL <paulinalll90@wp.pl>
2020-05-29 17:08:08 +0200

    Call DeleteNote component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import NotesList from "./components/notes-list";
 import AddNote from "./components/add-note";
+import DeleteNote from "./components/delete-note";
 
 function App() {
   return (
     <div className="App">
       <AddNote />
       <NotesList title="My notes list" testProp="hola!" />
+      <DeleteNote />
     </div>
   );
 }
